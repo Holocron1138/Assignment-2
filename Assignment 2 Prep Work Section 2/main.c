39acaf1d73aabe4163ea3e2bcc5a22007018b98c
Adam Goldberg <adammgoldberg@hotmail.com>
2015-09-14 17:12:49 -0400

    Initial Commit

---
+++ Assignment 2 Prep Work Section 2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Assignment 2 Prep Work Section 2
+//
+//  Created by Adam Goldberg on 2015-09-14.
+//  Copyright (c) 2015 Adam Goldberg. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
